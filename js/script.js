9df9f295a7901935f732ef19960f291b3d7077ab
tosmel2 <adewaletosin0808@gmail.com>
2022-02-21 13:53:54 +0100

    preloader functionality fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 AOS.init({
     duration: 1200,
 })
+
+
+//loader function
+function loadLoader(){
+    setTimeout(loader , 3000);
+}
+
+function loader(){
+    var loaderContainer = document.querySelector('.loader')
+    loaderContainer.style.display = 'none';
+
+}
+
+window.addEventListener('load' , loadLoader);
